def01478c254154aa2bbcb1ac4a4541d9a4167d2
mohamedagm <abogm44ud@gmail.com>
2025-04-12 03:33:19 +0200

    create edit_note_view

--- lib/views/widgets/notes_item.dart
+++ lib/views/widgets/notes_item.dart
@@ -1,51 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:notes_v1/views/edit_note_view.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 12.0),
-      child: Container(
-        padding: EdgeInsets.only(left: 16, top: 16, bottom: 16),
-        decoration: BoxDecoration(
-          color: Colors.orangeAccent,
-          borderRadius: BorderRadius.circular(16),
-        ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 12.0),
+        child: Container(
+          padding: EdgeInsets.only(left: 16, top: 16, bottom: 16),
+          decoration: BoxDecoration(
+            color: Colors.orangeAccent,
+            borderRadius: BorderRadius.circular(16),
+          ),
 
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              //tileColor: Colors.orangeAccent,
-              title: Text(
-                'data',
-                style: TextStyle(color: Colors.black, fontSize: 26),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  'data sbbeqb bbbbbsdwqbb 000000000000000000000000000bbo bbbb',
-                  style: TextStyle(
-                    color: const Color.fromRGBO(0, 0, 0, 0.7),
-                    fontSize: 16,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                //tileColor: Colors.orangeAccent,
+                title: Text(
+                  'data',
+                  style: TextStyle(color: Colors.black, fontSize: 26),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    'data sbbeqb bbbbbsdwqbb 000000000000000000000000000bbo bbbb',
+                    style: TextStyle(
+                      color: const Color.fromRGBO(0, 0, 0, 0.7),
+                      fontSize: 16,
+                    ),
                   ),
                 ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.delete, color: Colors.black),
+                ),
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.delete, color: Colors.black),
+              Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Text(
+                  '16May, 2004',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 16),
-              child: Text(
-                '16May, 2004',
-                style: TextStyle(color: Colors.black, fontSize: 16),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
