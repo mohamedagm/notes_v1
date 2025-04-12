61e78de25758145dcfce967fc9906244f5d06313
mohamedagm <abogm44ud@gmail.com>
2025-04-13 00:03:10 +0200

    finish edit_note + finish Ui

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_v1/views/widgets/custom_app_bar.dart';
 import 'package:notes_v1/views/widgets/edit_note_view_body.dart';
 
 class EditNoteView extends StatelessWidget {
--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:notes_v1/views/widgets/custom_app_bar.dart';
+import 'package:notes_v1/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 50),
-        CustomAppBar(text: 'Edit note', iconData: Icons.check),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          CustomAppBar(text: 'Edit note', iconData: Icons.check),
+          SizedBox(height: 25),
+          CustomTextField(onChanged: (value) {}, hint: 'title'),
+          SizedBox(height: 25),
+          CustomTextField(onChanged: (value) {}, hint: 'content', maxLines: 5),
+        ],
+      ),
     );
   }
 }
